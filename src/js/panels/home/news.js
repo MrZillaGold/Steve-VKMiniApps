a351e1e4e7e179b08a176998d4de7ba3ee1f4ad4
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-08-07 20:54:57 +1000

    Update news.js

--- src/js/panels/home/news.js
+++ src/js/panels/home/news.js
@@ -28,35 +28,29 @@ class NewsGet extends React.Component {
     };
 
     newsGet() {
-        let script = document.createElement('script');
-        script.src = "https://vk.com/js/api/openapi.js?136";
-        document.body.appendChild(script);
+        VKConnect.send("VKWebAppCallAPIMethod", {"method": "wall.get", "params": { "owner_id": "-155462018", "count": "1", "extended": "1", "v": "5.60", "access_token": "62fab35762fab35762fab3577862a5cf51662fa62fab3573fbd7436a10b52e8f0045684"}})
+            .then(data => {
+                let currentDate = new Date(data.data.response.items[0].date * 1000);
 
-        script.onload = function() {
-            VK.init({
-                apiId: 7078246
-            });
-            VK.Api.call('wall.get', {owner_id: -155462018, count: 1, extended: 1, v: "5.60"}, function(data) {
-                if (data.response) {
+                let getDate = currentDate.getDate();
+                let getMonth = currentDate.getMonth() + 1;
+                let getYear = currentDate.getFullYear();
 
-                    let currentDate = new Date(data.response.items[0].date * 1000);
-
-                    let getDate = currentDate.getDate();
-                    let getMonth = currentDate.getMonth() + 1;
-                    let getYear = currentDate.getFullYear();
-
-                    let month = getMonth > 9 ? getMonth : `0` + getMonth;
-                    let date = getDate > 9 ? getDate : `0` + getDate;
-                    let newsTitle = date + '.' + month + '.' + getYear;
-
-                    this.setState({
-                        news: data.response.items[0].text.replace(/\n/g, '<br />'),
-                        time: newsTitle,
-                        spinner: false
-                    });
-                }
-            });
-        }
+                let month = getMonth > 9 ? getMonth : `0` + getMonth;
+                let date = getDate > 9 ? getDate : `0` + getDate;
+                let newsTitle = date + '.' + month + '.' + getYear;
+                this.setState({
+                    news: data.data.response.items[0].text.replace(/\n/g, '<br />'),
+                    image: data.data.response.items[0].attachments[0].photo.photo_1280,
+                    time: newsTitle,
+                    spinner: false
+                });
+            }).catch(err => {
+            if (err) {
+                this.setState({error: `Новости сейчас недоступны!`, spinner: false});
+                return console.log(err);
+            }
+        });
     }
 
     render() {
