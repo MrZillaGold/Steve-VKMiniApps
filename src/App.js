c23fbd1a5d38cd7bb45a729a0b4fae87d7dad761
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-08-08 22:42:56 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import {bindActionCreators} from 'redux'
 import {goBack, closeModal} from "./js/store/router/actions";
 import * as VK from './js/services/VK';
 
+import { Offline, Online } from 'react-detect-offline';
+
 import View from '@vkontakte/vkui/dist/components/View/View';
 import Root from '@vkontakte/vkui/dist/components/Root/Root';
 import ConfigProvider from '@vkontakte/vkui/dist/components/ConfigProvider/ConfigProvider';
+import Gallery from "@vkontakte/vkui/dist/components/Gallery/Gallery";
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
+import Cell from "@vkontakte/vkui/dist/components/Cell/Cell";
+import Div from "@vkontakte/vkui/dist/components/Div/Div";
+
 
 import HomePanelProfile from './js/panels/home/base';
 import HomePanelServer from './js/panels/home/server';
-import HomePanelNameHistory from './js/panels/home/namehistory';
-import HomePanelSkin from './js/panels/home/skin';
+import HomePanelUser from './js/panels/home/user';
 import HomePanelNews from './js/panels/home/news';
 import HomePanelStatus from './js/panels/home/status';
+import HomePanelCrafts from './js/panels/home/crafts';
 
 class App extends React.Component {
     constructor(props) {
@@ -62,21 +69,42 @@ class App extends React.Component {
 
         return (
             <ConfigProvider isWebView={true} scheme={colorScheme}>
-                <Root activeView={activeView} popout={popout}>
-                    <View
-                        id="home"
-                        activePanel={activePanel}
-                        history={history}
-                        onSwipeBack={() => goBack()}
-                    >
-                        <HomePanelProfile id="base" />
-                        <HomePanelServer id="server"/>
-                        <HomePanelNameHistory id="namehistory"/>
-                        <HomePanelSkin id="skin"/>
-                        <HomePanelNews id="news"/>
-                        <HomePanelStatus id="status"/>
-                    </View>
-                </Root>
+                <Online>
+                    <Root activeView={activeView} popout={popout}>
+                        <View
+                            id="home"
+                            activePanel={activePanel}
+                            history={history}
+                            onSwipeBack={() => goBack()}
+                        >
+                            <HomePanelProfile id="base" />
+                            <HomePanelServer id="server"/>
+                            <HomePanelUser id="user"/>
+                            <HomePanelNews id="news"/>
+                            <HomePanelStatus id="status"/>
+                            <HomePanelCrafts id="crafts"/>
+                        </View>
+                    </Root>
+                </Online>
+                <Offline>
+                    <Div style={{ userSelect: 'none', marginTop: '56px' }}>
+                        <Cell align='center'><b>Упс...</b></Cell>
+                        <p style={{ whiteSpace: 'pre-wrap', color: '#909499', textAlign: 'center' }}>
+                            Пропало подключение с сервером!<br /><br />Сервис автоматически переподключится как появится соединение.
+                        </p>
+                        <Button level='tertiary' stretched component='a' href='https://vk.com/stevebotmc'>Группа</Button>
+                        <Gallery
+                            style={{ height: 200 }}
+                        >
+                            <div style={{
+                                backgroundImage: 'url(https://psv4.userapi.com/c848424/u233731786/docs/d8/5b1e5e8f3fa5/Enderman.png)',
+                                backgroundSize: 'contain',
+                                backgroundPosition: '50%',
+                                backgroundRepeat: 'no-repeat'}}
+                            />
+                        </Gallery>
+                    </Div>
+                </Offline>
             </ConfigProvider>
         );
     }
