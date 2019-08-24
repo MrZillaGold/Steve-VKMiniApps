17c5fdfc6ce3abe467dc7797fe4504a57802b08a
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-08-24 10:21:43 +1000

    Update calculator.js

--- src/js/panels/home/calculator.js
+++ src/js/panels/home/calculator.js
@@ -9,23 +9,32 @@ import { Panel, PanelHeader, PanelHeaderBack, PanelHeaderContent, Input, FormLay
 class Calculator extends React.Component {
 
     state = {
-        x: null,
-        y: null,
-        z: null,
+        x: "",
+        y: "",
+        z: "",
         spinner: null,
         error: null,
         rand: null,
-        world: null
+        world: "world"
     };
 
     onChange(e) {
         const {name, value} = e.currentTarget;
+        if (name === "x" || name === "z") {
+            return this.setState({[name]: value.replace(/[^-0-9]+$/g, "").slice(0, 9) > 0 || value.replace(/[^-0-9]+$/g, "").slice(0, 9) === 0 ?
+                        value.replace(/[^-0-9]+$/g, "").slice(0, 8) > 29999999 ? "29999999" : value.replace(/[^-0-9]+$/g, "").slice(0, 8)
+                        :
+                        value.replace(/[^-0-9]+$/g, "").slice(0, 9) < -29999999 ? "-29999999" : value.replace(/[^-0-9]+$/g, "").slice(0, 9)});
+        }
+        if (name === "y") {
+            return this.setState({[name]: value.replace(/[^0-9]+$/g, "").slice(0, 6) > 100000 ? "100000" : value.replace(/[^0-9]+$/g, "").slice(0, 6)});
+        }
         this.setState({[name]: value});
     }
 
     render() {
 
-        const {x, y, z, world, id, goBack} = this.props;
+        const {world, id, goBack} = this.props;
 
         return (
             <Panel id={id}>
@@ -35,64 +44,64 @@ class Calculator extends React.Component {
                     </PanelHeaderContent>
                 </PanelHeader>
                 <FormLayout>
-                    <Select name="world" onChange={this.onChange.bind(this)} value={world} top="Мир" placeholder="Выберите мир" bottom='Мир, для которого необходимо просчитать координаты.'>
+                    <Select name="world" onChange={this.onChange.bind(this)} value={world} top="Мир*" bottom='* Для которого необходимо просчитать координаты.'>
                         <option value="world">Обычный</option>
                         <option value="nether">Ад</option>
                     </Select>
 
                     <Input
                         top='Координата X'
-                        bottom='Целое число.'
-                        type="number"
+                        bottom='Целое число. (-29999999 — 29999999)'
                         name='x'
-                        value={x}
+                        value={this.state.x}
+                        status={this.state.x.match(/^-?[0-9]+$/g) || this.state.x === "" ? 'default' : 'error'}
                         onChange={this.onChange.bind(this)}
                         placeholder="220"
-                        maxLength='7'
-                        pattern='^[-]?[0-9]+$'
+                        autocomplete="off"
+                        pattern='^-?[0-9]+'
                     />
                     <Input
                         top='Координата Y'
-                        bottom='Целое число.'
-                        type="number"
+                        bottom='Целое число. (0 — 100000)'
                         name='y'
-                        value={y}
+                        value={this.state.y}
+                        status={this.state.y.match(/^[0-9]+$/g) || this.state.y === "" ? 'default' : 'error'}
                         onChange={this.onChange.bind(this)}
                         placeholder="64"
-                        maxLength='3'
+                        autocomplete="off"
                         pattern='^[0-9]+$'
                     />
                     <Input
                         top='Координата Z'
-                        bottom='Целое число.'
-                        type="number"
+                        bottom='Целое число. (-29999999 — 29999999)'
                         name='z'
-                        value={z}
+                        value={this.state.z}
+                        status={this.state.z.match(/^-?[0-9]+$/g) || this.state.z === "" ? 'default' : 'error'}
                         onChange={this.onChange.bind(this)}
                         placeholder="-113"
-                        maxLength='7'
-                        pattern='^[-]?[0-9]+$'
+                        autocomplete="off"
+                        pattern='^-?[0-9]+'
                     />
-                    {this.state.world === null ? '' :
+                    {
                         <List top={this.state.world === 'nether' ? 'Координаты в аду' : 'Координаты в обычном мире'}>
                             <Cell description='Координата X'>
-                                {this.state.x === null ? '0' : this.state.world === 'nether' ? Math.floor(this.state.x / 8) : Math.floor(this.state.x * 8)}
+                                {this.state.x === '' ? '0' : this.state.x.match('^-?[0-9]+$') ? this.state.world === 'nether' ? Math.floor(this.state.x / 8) : Math.floor(this.state.x * 8) : 'Задано неверное значение'}
                             </Cell>
                             <Cell description='Координата Y'>
-                                {this.state.y === null ? '0' : this.state.y.match('^[0-9]+$') ? this.state.y : 0}
+                                {this.state.y === '' ? '0' : this.state.y.match('^[0-9]+$') ? this.state.y : 'Задано неверное значение'}
                             </Cell>
                             <Cell description='Координата Z'>
-                                {this.state.z === null ? '0' : this.state.world === 'nether' ? Math.floor(this.state.z / 8) : Math.floor(this.state.z * 8)}
+                                {this.state.z === '' ? '0' : this.state.z.match('^-?[0-9]+$') ? this.state.world === 'nether' ? Math.floor(this.state.z / 8) : Math.floor(this.state.z * 8) : 'Задано неверное значение'}
                             </Cell>
                         </List>
                     }
-                    {this.state.world === null ? '' :
+                    {
                         <List>
                             <Cell description='Номер чанка'>
-                                {`${this.state.x === null ? '0' : Math.floor(this.state.x / 16)}, ${this.state.y === null ? '0' : Math.floor(this.state.y / 16)}, ${this.state.z === null ? '0' : Math.floor(this.state.z / 16)}`}
+                                {`${this.state.x === '' ? '0' : this.state.x.match('^-?[0-9]+$') ? Math.floor(this.state.x / 16) : '0'}, ${this.state.y === '' ? '0' : this.state.y.match('^[0-9]+$') ? Math.floor(this.state.y / 16) : '0'}, ${this.state.z === '' ? '0' : this.state.z.match('^-?[0-9]+$') ? Math.floor(this.state.z / 16) : '0'}`}
                             </Cell>
                             <Cell description='Файл чанка'>
-                                {`r.${this.state.x === null ? '0' : Math.floor(this.state.x / 512)}.${this.state.z === null ? '0' : Math.floor(this.state.z / 512)}.mca`}
+                                {`r.${this.state.x === '' ? '0' : this.state.x.match('^-?[0-9]+$') ? Math.floor(this.state.x / 512) : '0'}.${this.state.z === '' ? '0' : this.state.z.match('^-?[0-9]+$') ? Math.floor(this.state.z / 512) : '0'}.mca`}
                             </Cell>
                         </List>
                     }
