b9447a834ec8379ade94d27bcb090572b12fa12c
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-09-07 17:34:29 +1000

    Функция конвертации времени в нормальный формат.

--- src/js/services/_functions.js
+++ src/js/services/_functions.js
@@ -10,3 +10,15 @@ export const smoothScrollToTop = () => {
         window.scrollTo(0, c - c / 8);
     }
 };
+
+export const timeConvert = (time) => {
+    const currentDate = new Date(time);
+
+    const getDate = currentDate.getDate();
+    const getMonth = currentDate.getMonth() + 1;
+    const getYear = currentDate.getFullYear();
+
+    const month = getMonth > 9 ? getMonth : `0` + getMonth;
+    const date = getDate > 9 ? getDate : `0` + getDate;
+    return date + '.' + month + '.' + getYear;
+};
