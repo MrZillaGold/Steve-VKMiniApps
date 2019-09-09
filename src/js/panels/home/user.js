8774af87a640e1cc1c749b9776634ea9b2c3cea3
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-09-09 22:19:31 +1000

    Фикс вставки текста в полях с ограничением.

--- src/js/panels/home/user.js
+++ src/js/panels/home/user.js
@@ -32,7 +32,7 @@ class UserGet extends React.Component {
 
     onChange(e) {
         const {name, value} = e.currentTarget;
-        this.setState({[name]: value.replace(/[^A-Za-z0-9_]+$/g, "").slice(0, 16)});
+        this.setState({[name]: value.replace(/[^A-Za-z0-9_]/g, "").slice(0, 16)});
     }
 
     onClick () {
@@ -104,7 +104,7 @@ class UserGet extends React.Component {
                                 bottom='Может содержать только латинские буквы, цифры и символ "_". (От 3 до 16 символов)'
                                 placeholder="Введите никнейм"
                                 maxLength='16'
-                                pattern='^[A-Za-z0-9_]+$'
+                                pattern='^[A-Za-z0-9_]'
                             />
                             :
                             <Input
