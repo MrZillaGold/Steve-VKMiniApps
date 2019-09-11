30b2f2aa7ca90692cc5d825b9d1b3f0e446abd38
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-09-11 20:47:30 +1000

    Update status.js

--- src/js/panels/home/status.js
+++ src/js/panels/home/status.js
@@ -4,12 +4,13 @@ import {bindActionCreators} from "redux";
 import axios from 'axios';
 
 import { Offline, Online } from 'react-detect-offline';
+import OfflineBlock from './offline';
 
 import {checkStatus} from "../../services/_functions";
 
 import {goBack, openPopout, closePopout, openModal} from "../../store/router/actions";
 
-import { Panel, PanelHeader, PanelHeaderBack, PanelHeaderContent, Spinner, Avatar, Group, Cell, List, Gallery, Div, Button } from "@vkontakte/vkui";
+import { Panel, PanelHeader, PanelHeaderBack, PanelHeaderContent, Spinner, Avatar, Group, Cell, List, Gallery, Div } from "@vkontakte/vkui";
 
 
 class StatusGet extends React.Component {
@@ -126,21 +127,7 @@ class StatusGet extends React.Component {
                     }
                 </Online>
                 <Offline>
-                    <Div style={{ userSelect: 'none', marginTop: '56px' }}>
-                        <Cell align='center'><b>Упс...</b></Cell>
-                        <p style={{ whiteSpace: 'pre-wrap', color: '#909499', textAlign: 'center' }}>
-                            Пропало подключение с сервером!<br /><br />Эта вкладка будет доступна как появится соединение.
-                        </p>
-                        <Button level='tertiary' stretched component='a' href='https://vk.com/stevebotmc'>Группа</Button>
-                        <Gallery style={{ height: 200 }}>
-                            <div style={{
-                                backgroundImage: 'url(https://www.minecraft.net/content/dam/archive/0ef629a3446f9a977087c578189097dd-sticker_creeper.png)',
-                                backgroundSize: 'contain',
-                                backgroundPosition: '50%',
-                                backgroundRepeat: 'no-repeat'}}
-                            />
-                        </Gallery>
-                    </Div>
+                    <OfflineBlock />
                 </Offline>
             </Panel>
         );
