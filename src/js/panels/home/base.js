d5178bf0b0ecbd0bb9ccfdf810d1fc1a7460aa46
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-09-13 00:33:27 +1000

    Калькулятор крепости

--- src/js/panels/home/base.js
+++ src/js/panels/home/base.js
@@ -11,6 +11,7 @@ import Icon24NewsFeed from '@vkontakte/icons/dist/24/newsfeed';
 import Icon24Globe from '@vkontakte/icons/dist/24/globe';
 import Icon24Write from '@vkontakte/icons/dist/24/write';
 import Icon24Forward10 from '@vkontakte/icons/dist/24/forward_10';
+import Icon24Search from '@vkontakte/icons/dist/24/search';
 
 class HomePanelBase extends React.Component {
     render() {
@@ -25,6 +26,7 @@ class HomePanelBase extends React.Component {
                     <Separator />
                     <CellButton before={<Icon24Write />} onClick={() => setPage('home', 'achievements')}>Генератор достижений</CellButton>
                     <CellButton before={<Icon24Forward10 />} onClick={() => setPage('home', 'calculator')}>Калькулятор координат</CellButton>
+                    <CellButton before={<Icon24Search />} onClick={() => setPage('home', 'endercalculator')}>Получение координат крепости</CellButton>
                     <Separator />
                     <CellButton before={<Icon24NewsFeed />} onClick={() => setPage('home', 'news')}>Новости Minecraft</CellButton>
                     <CellButton before={<Icon24Globe />} onClick={() => setPage('home', 'status')}>Состояние серверов Minecraft</CellButton>
