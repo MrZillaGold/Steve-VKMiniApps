3fd12a5f5f6008ebf40493b6ab368ea4d71a652e
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-09-29 22:50:58 +1000

    Update calculator.js

--- src/js/panels/home/calculator.js
+++ src/js/panels/home/calculator.js
@@ -23,12 +23,6 @@ class Calculator extends React.Component {
     };
 
     onChange(e) {
-        document.querySelector("input").addEventListener("keydown", function(e) {
-            if (e.keyCode === 13) {
-                document.querySelector("input").setAttribute("readonly", "readonly");
-                setTimeout(function(){document.querySelector("input").removeAttribute("readonly")}, 500)
-            }
-        });
         this.setState({ copy: false });
 
         const {name, value} = e.currentTarget;
