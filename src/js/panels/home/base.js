9a2c1f9888b3e5513de3aa40ed45389a899e1f21
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-10-06 03:21:22 +1000

    Редизайн

--- src/js/panels/home/base.js
+++ src/js/panels/home/base.js
@@ -3,37 +3,39 @@ import {connect} from 'react-redux';
 
 import {closePopout, goBack, openModal, openPopout, setPage} from '../../store/router/actions';
 
-import { Panel, Group, Button, PanelHeader, Cell, CellButton, Avatar, Separator } from '@vkontakte/vkui';
+import { Panel, Group, Button, PanelHeader, Cell, Avatar, Separator } from '@vkontakte/vkui';
 
 import Icon24Globe from '@vkontakte/icons/dist/24/globe';
 import Icon24Write from '@vkontakte/icons/dist/24/write';
 import Icon24Search from '@vkontakte/icons/dist/24/search';
+import "./custom.css";
 
 class HomePanelBase extends React.Component {
+
     render() {
         const {id, setPage, eruda} = this.props;
 
         return (
             <Panel id={id}>
-                <PanelHeader>Steve</PanelHeader>
+                <PanelHeader transparent>Steve</PanelHeader>
                 <Group>
-                    <CellButton before={<div style={{height: '24px', width: '36px'}}><img src={require('./img/usericon.svg')} alt="Server Info"/></div>} onClick={() => setPage('home', 'user')}>Информация об игроке</CellButton>
-                    <CellButton before={<div style={{height: '24px', width: '36px'}}><img src={require('./img/servericon.svg')} alt="Server Info"/></div>} onClick={() => setPage('home', 'server')}>Информация о сервере по IP</CellButton>
+                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/usericon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'user')}>Информация об игроке</Cell>
+                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/servericon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'server')}>Информация о сервере по IP</Cell>
                     <Separator />
-                    <CellButton before={<Icon24Write />} onClick={() => setPage('home', 'achievements')}>Генератор достижений</CellButton>
-                    <CellButton before={<div style={{height: '24px', width: '36px'}}><img src={require('./img/calculatoricon.svg')} alt="Server Info"/></div>} onClick={() => setPage('home', 'calculator')}>Калькулятор координат</CellButton>
-                    <CellButton before={<Icon24Search />} onClick={() => setPage('home', 'endercalculator')}>Получение координат крепости</CellButton>
+                    <Cell before={<Icon24Write />} onClick={() => setPage('home', 'achievements')}>Генератор достижений</Cell>
+                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/calculatoricon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'calculator')}>Калькулятор координат</Cell>
+                    <Cell before={<Icon24Search />} onClick={() => setPage('home', 'endercalculator')}>Получение координат крепости</Cell>
                     <Separator />
-                    <CellButton before={<Icon24Globe />} onClick={() => setPage('home', 'status')}>Состояние серверов Minecraft</CellButton>
+                    <Cell before={<Icon24Globe />} onClick={() => setPage('home', 'status')}>Состояние серверов Minecraft</Cell>
                 </Group>
                 <Group>
                     <Cell
                         multiline
                         before={<Avatar type='image' onDoubleClick={eruda} size={64} src={require('./img/Steve.png')} style={{backgroundColor: 'transparent'}}/>}
                         size="l"
-                        description="Получите быстрый доступ ко всем функциям в сообщениях ВКонтакте!"
+                        description="Получите быстрый доступ ко всем функциям в сообщениях ВК!"
                         bottomContent={
-                            <Button component="a" target="_blank" href="https://vk.com/public175914098">Перейти в группу с ботом</Button>
+                            <Button component="a" target="_blank" href="https://vk.com/public175914098"><b>Перейти в группу с ботом</b></Button>
                         }
                     >
                         Steve Бот
