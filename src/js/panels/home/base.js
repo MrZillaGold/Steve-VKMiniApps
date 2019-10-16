673e33f4ceddf8738597ae7bad28425f85244370
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-10-17 00:08:43 +1000

    Pointer и нормальный дизайн кнопок.

--- src/js/panels/home/base.js
+++ src/js/panels/home/base.js
@@ -4,11 +4,11 @@ import {connect} from 'react-redux';
 import {closePopout, goBack, openModal, openPopout, setPage} from '../../store/router/actions';
 
 import { Panel, Group, Button, PanelHeader, Cell, Avatar, Separator } from '@vkontakte/vkui';
+import "./scss/styles.scss"
 
 import Icon24Globe from '@vkontakte/icons/dist/24/globe';
 import Icon24Write from '@vkontakte/icons/dist/24/write';
 import Icon24Search from '@vkontakte/icons/dist/24/search';
-import "./custom.css";
 
 class HomePanelBase extends React.Component {
 
@@ -19,14 +19,14 @@ class HomePanelBase extends React.Component {
             <Panel id={id}>
                 <PanelHeader transparent>Steve</PanelHeader>
                 <Group>
-                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/usericon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'user')}>Информация об игроке</Cell>
-                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/servericon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'server')}>Информация о сервере по IP</Cell>
+                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/usericon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'user')} className="pointer">Информация об игроке</Cell>
+                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/servericon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'server')} className="pointer">Информация о сервере по IP</Cell>
                     <Separator />
-                    <Cell before={<Icon24Write />} onClick={() => setPage('home', 'achievements')}>Генератор достижений</Cell>
-                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/calculatoricon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'calculator')}>Калькулятор координат</Cell>
-                    <Cell before={<Icon24Search />} onClick={() => setPage('home', 'endercalculator')}>Получение координат крепости</Cell>
+                    <Cell before={<Icon24Write />} onClick={() => setPage('home', 'achievements')} className="pointer">Генератор достижений</Cell>
+                    <Cell before={<div style={{height: '24px', width: '40px'}}><img src={require('./img/calculatoricon.svg')} className="icon_base" alt="Server Info"/></div>} onClick={() => setPage('home', 'calculator')} className="pointer">Калькулятор координат</Cell>
+                    <Cell before={<Icon24Search />} onClick={() => setPage('home', 'endercalculator')} className="pointer">Получение координат крепости</Cell>
                     <Separator />
-                    <Cell before={<Icon24Globe />} onClick={() => setPage('home', 'status')}>Состояние серверов Minecraft</Cell>
+                    <Cell before={<Icon24Globe />} onClick={() => setPage('home', 'status')} className="pointer">Состояние серверов Minecraft</Cell>
                 </Group>
                 <Group>
                     <Cell
