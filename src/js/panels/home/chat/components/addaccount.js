eec187cb0844c12ef05340b118d837a3d947a984
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-11-03 18:56:51 +1000

    Скрываем модалку после потери соединения

--- src/js/panels/home/chat/components/addaccount.js
+++ src/js/panels/home/chat/components/addaccount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Online} from 'react-detect-offline';
 import {Button, FormLayout, Group, Input, ModalPage, Tabs, TabsItem} from "@vkontakte/vkui";
 
 class AddAccount extends React.Component {
@@ -18,7 +19,7 @@ class AddAccount extends React.Component {
         this.setState({[name]: value, error: false});
     }
 
-    async login() {
+    login() {
         if (this.state.type === "license") {
             this.setState({loading: true});
             this.props.navigator.params.socket.emit('server:connect', {
@@ -54,12 +55,13 @@ class AddAccount extends React.Component {
     }
 
     render() {
-        const {id, header, onClose} = this.props;
+        const {id, header, onClose, navigator} = this.props;
         const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
         const nicknameRegExp = /^[A-Za-z0-9_]/g;
 
         return (
-            <ModalPage id={id} header={header} onClose={onClose}>
+            <Online polling={{interval: 1000}} onChange={() => navigator.hideModal()}>
+                <ModalPage id={id} header={header} onClose={onClose}>
                     <Group title="Тип аккаунта">
                         <Tabs type="buttons">
                             <TabsItem
@@ -76,64 +78,65 @@ class AddAccount extends React.Component {
                             </TabsItem>
                         </Tabs>
                     </Group>
-                        <FormLayout>
-                            {
-                                this.state.type === "license" ?
-                                    <Input
-                                        top="Электронная почта"
-                                        autoComplete="off"
-                                        name="email"
-                                        disabled={this.state.spinner}
-                                        value={this.state.email}
-                                        onChange={this.onChange.bind(this)}
-                                        status={this.state.email.match(emailRegExp) || this.state.email === "" ? 'default' : 'error'}
-                                        bottom={this.state.email.match(emailRegExp) || this.state.email === "" ? '' : 'Неверный адрес электронной почты!'}
-                                    />
-                                    :
-                                    <Input
-                                        top = "Никнейм"
-                                        autoComplete="off"
-                                        name="nickname"
-                                        disabled={this.state.spinner}
-                                        value={this.state.nickname}
-                                        status={this.state.nickname === "" || this.state.nickname.length > 2 ? 'default' : 'error'}
-                                        bottom={`Может содержать только латинские буквы, цифры и символ "_". (От 3 до 16 символов)`}
-                                        onChange={this.onChange.bind(this)}
-                                    />
-                            }
-                            {
-                                this.state.type === "license" ?
-                                    <Input
-                                        top="Пароль"
-                                        autoComplete="off"
-                                        name="password"
-                                        type="password"
-                                        disabled={this.state.spinner}
-                                        value={this.state.password}
-                                        onChange={this.onChange.bind(this)}
-                                    />
-                                    :
-                                    undefined
-                            }
-                            {
-                                this.state.type === "pirate" ?
-                                    <div className="FormLayout__row-bottom">При использовании данного типа аккаунта, вы не сможете подключаться к лицензионным серверам.</div>
-                                    :
-                                    undefined
-                            }
-                            {
-                                this.state.error && this.state.type === "license" ?
+                    <FormLayout>
+                        {
+                            this.state.type === "license" ?
+                                <Input
+                                    top="Электронная почта"
+                                    autoComplete="off"
+                                    name="email"
+                                    disabled={this.state.spinner}
+                                    value={this.state.email}
+                                    onChange={this.onChange.bind(this)}
+                                    status={this.state.email.match(emailRegExp) || this.state.email === "" ? 'default' : 'error'}
+                                    bottom={this.state.email.match(emailRegExp) || this.state.email === "" ? '' : 'Неверный адрес электронной почты!'}
+                                />
+                                :
+                                <Input
+                                    top = "Никнейм"
+                                    autoComplete="off"
+                                    name="nickname"
+                                    disabled={this.state.spinner}
+                                    value={this.state.nickname}
+                                    status={this.state.nickname === "" || this.state.nickname.length > 2 ? 'default' : 'error'}
+                                    bottom={`Может содержать только латинские буквы, цифры и символ "_". (От 3 до 16 символов)`}
+                                    onChange={this.onChange.bind(this)}
+                                />
+                        }
+                        {
+                            this.state.type === "license" ?
+                                <Input
+                                    top="Пароль"
+                                    autoComplete="off"
+                                    name="password"
+                                    type="password"
+                                    disabled={this.state.spinner}
+                                    value={this.state.password}
+                                    onChange={this.onChange.bind(this)}
+                                />
+                                :
+                                undefined
+                        }
+                        {
+                            this.state.type === "pirate" ?
+                                <div className="FormLayout__row-bottom">При использовании данного типа аккаунта, вы не сможете подключаться к лицензионным серверам.</div>
+                                :
+                                undefined
+                        }
+                        {
+                            this.state.error && this.state.type === "license" ?
                                 <div style={{color: "#e64646", height: "24px"}} className="FormLayout__row-bottom">
                                     Неверный логин или пароль!
                                 </div>
-                                    :
-                                    undefined
-                            }
-                            <Button onClick={this.login.bind(this)} disabled={this.state.type === "license" ? this.state.email === "" || !this.state.email.match(emailRegExp) || this.state.password === "" || this.state.loading : this.state.nickname === "" || !this.state.nickname.match(nicknameRegExp) || this.state.nickname.length < 3} style={{marginBottom: "80px"}} size='xl'>
-                                <b>{this.state.loading ? "Авторизация...": "Добавить аккаунт"}</b>
-                            </Button>
-                        </FormLayout>
-            </ModalPage>
+                                :
+                                undefined
+                        }
+                        <Button onClick={() => this.login.bind(this)} disabled={this.state.type === "license" ? this.state.email === "" || !this.state.email.match(emailRegExp) || this.state.password === "" || this.state.loading : this.state.nickname === "" || !this.state.nickname.match(nicknameRegExp) || this.state.nickname.length < 3} style={{marginBottom: "80px"}} size='xl'>
+                            <b>{this.state.loading ? "Авторизация...": "Добавить аккаунт"}</b>
+                        </Button>
+                    </FormLayout>
+                </ModalPage>
+            </Online>
         );
     }
 
