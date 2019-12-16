2ae661a30c29ade8659dfb3c087d9402ef06d747
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2019-12-16 18:53:38 +1000

    Revert "Update App.js"

--- src/App.js
+++ src/App.js
@@ -24,7 +24,26 @@ class App extends React.Component {
         await this.setState({eruda: !this.state.eruda});
         this.state.eruda ? window.eruda.init() : window.eruda.destroy()
     };
-    
+
+    hash() {
+        if (window.location.hash) {
+            const hash = window.location.hash.match(/#(.+?)(?:=([^]+)|$)/);
+            const panels = ["user", "server", "status", "achievements", "calculator", "endercalculator", "chat"];
+            if (hash[1] && panels.includes(hash[1])) {
+                if (hash[1] === "user" && hash[2] && hash[2].match(/^[A-Za-z0-9_]{3,16}$/g)) {
+                    return {panel: "user", data: hash[2]};
+                } else {
+                    return {panel: hash[1]};
+                }
+            } else {
+                return {panel: "home"};
+            }
+        } else {
+            return {panel: "home"};
+        }
+    }
+
+
     render() {
         return (
             <Stack
@@ -39,7 +58,7 @@ class App extends React.Component {
                             title="Добавление аккаута"
                         />
                 ]}>
-                <Page id="main" activePanel="home">
+                <Page id="main" activePanel={this.hash().panel}>
                     <Home id="home" eruda={this.eruda}/>
                     <Server id="server"/>
                     <User id="user"/>
