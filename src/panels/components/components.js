ad055b0f6b8aeefa47c4fec4719743c4c4a2b5b7
Egor <mrzillagold@gmail.com>
2020-03-13 18:47:16 +1000

    Импорт компонентов в отдельном файле

---
+++ src/panels/components/components.js
@@ -0,0 +1,13 @@
+import Error from "./error";
+import HeaderButtons from "./headerbuttons";
+import OfflineBlock from "./offline";
+import SkinViewer from "./skinviewer";
+import Spinner from "./spinner";
+
+export {
+    Error,
+    HeaderButtons,
+    OfflineBlock,
+    SkinViewer,
+    Spinner
+};
