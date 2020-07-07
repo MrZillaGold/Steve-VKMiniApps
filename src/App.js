24d826ae36a5315524d31d2835c02aa291f8e974
Egor <mrzillagold@gmail.com>
2020-07-08 01:33:31 +1000

    Передача isWebView для Stack

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Stack, Page } from "vkui-navigator/dist"
+import React from "react";
+import VKBridge from "@vkontakte/vk-bridge";
+import { Stack, Page } from "vkui-navigator/dist";
 
 import { Home } from "./panels/Home";
-import { ServerInfo } from './panels/ServerInfo';
-import { UserInfo } from './panels/UserInfo';
-import { MojangStatus } from './panels/MojangStatus';
-import { AchievementGenerator } from './panels/AchievementGenerator';
-import { Calculator } from './panels/Calculator';
+import { ServerInfo } from "./panels/ServerInfo";
+import { UserInfo } from "./panels/UserInfo";
+import { MojangStatus } from "./panels/MojangStatus";
+import { AchievementGenerator } from "./panels/AchievementGenerator";
+import { Calculator } from "./panels/Calculator";
 
 export function App() {
     return (
         <Stack webviewType="vkapps"
                activePage="main"
+               isWebView={VKBridge.isWebView()}
         >
             <Page id="main" activePanel="home">
                 <Home id="home"/>
