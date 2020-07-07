c057ae466220ec8b37e758c4cae1d1cb5f9fc368
Egor <mrzillagold@gmail.com>
2020-07-08 01:33:58 +1000

    Проверка платформы для смены цвета статус-бара

--- src/bridge.js
+++ src/bridge.js
@@ -4,7 +4,7 @@ import getArgs from "vkappsutils/src/Args";
 function changeStatusBarColor() {
     const { platform } = getArgs();
 
-    if (platform !== "desktop_web") {
+    if (platform !== "desktop_web" && platform !== "mobile_web") {
         VKBridge.send("VKWebAppSetViewSettings", {
             status_bar_style: "light", action_bar_color: "#1c1c1c"
         });
