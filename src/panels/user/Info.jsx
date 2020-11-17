aee30c4fae17a6fa265c5e7dec88b6c753693034
Пронин Егор <mrzillagold@gmail.com>
2020-11-18 01:59:47 +1000

    Исправление условия

--- src/panels/user/Info.jsx
+++ src/panels/user/Info.jsx
@@ -20,7 +20,7 @@ export function Info({ user, setUser, spinner, error }) {
                 {
                     user ?
                         <Group mode="plain">
-                            <Tabs style={viewWidth >= ViewWidth.TABLET ? { marginTop: 0, marginBottom: 8 } : {}}>
+                            <Tabs style={viewWidth <= ViewWidth.SMALL_TABLET ? { marginTop: 0, marginBottom: 8 } : {}}>
                                 <TabsItem
                                     onClick={() => setActiveTab("skin")}
                                     selected={activeTab === "skin"}
