7f1f8bd599409d343c3d16ae446de75cc83c1ffa
Пронин Егор <mrzillagold@gmail.com>
2021-02-03 16:43:27 +1000

    Отступ для предпросмотра скина

--- src/panels/user/Skin.jsx
+++ src/panels/user/Skin.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Group } from "@vkontakte/vkui";
+import { Group, Div } from "@vkontakte/vkui";
 
 import { SkinPreview } from "../../components";
-
 import { Skins } from "./Skins";
 
 export function Skin({ user, setUser }) {
+
     return (
         <>
             <Group mode="plain">
-                <SkinPreview skin={user.skin.url}
-                             cape={user.skin.cape ? `data:image/png;base64,${user.skin.cape}` : ""}
-                             isSlim={user.skin.isSlim}
-                             username={user.username}
-                             style={{ borderRadius: 10 }}
-                />
+                <Div>
+                    <SkinPreview skin={user.skin.url}
+                                 cape={user.skin.cape ? `data:image/png;base64,${user.skin.cape}` : ""}
+                                 isSlim={user.skin.isSlim}
+                                 username={user.username}
+                                 style={{ borderRadius: 10 }}
+                    />
+                </Div>
             </Group>
             {
                 user.skin.history.length && <Skins user={user} setUser={setUser}/>
             }
         </>
-    )
+    );
 }
