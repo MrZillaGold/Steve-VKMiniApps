ab0028bfc666f217bcb7f2d3baca0d4992f535a5
Пронин Егор <mrzillagold@gmail.com>
2021-03-30 07:51:12 +1000

    Нижний отступ

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -11,7 +11,7 @@ export function UserCard({ user }) {
                     className="user-avatar"
             />
         }
-              style={{ marginTop: "8px" }}
+              style={{ margin: "8px 0" }}
               description={
                   user.rank_formatted && user.rank_formatted !== "&7" ?
                       user.rank_formatted.replace(/&./g, "")
