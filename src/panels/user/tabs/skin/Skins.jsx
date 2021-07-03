e11e2507ffb7d9fe93556dd7fe8013ab5dad7705
Egor <mrzillagold@gmail.com>
2021-07-03 11:33:34 +1000

    feat(Skins): ability for open skin preview modal

--- src/panels/user/tabs/skin/Skins.jsx
+++ src/panels/user/tabs/skin/Skins.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Div, Group, Header, Spinner } from "@vkontakte/vkui";
+import { useRouter } from "@unexp/router";
 
 import "./Skins.css";
 
 export function Skins({ user, setUser }) {
+
+    const { push } = useRouter();
 
     const selectSkin = (index) => {
         if (user.textures.skin.selected !== index) {
@@ -22,15 +25,22 @@ export function Skins({ user, setUser }) {
             </Header>
             <Div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
                 {
-                    user.textures.skin.history.map((skin, index) =>
+                    user.textures.skin.history.map((skin, index) => (
                         <img key={skin.url}
                              className={`Skin-Head ${user.textures.skin.selected === index && "Skin-Head_selected"}`}
                              src={skin.renders.face}
-                             onClick={() => selectSkin(index)}
+                             onClick={() => {
+                                 selectSkin(index);
+
+                                 push({ modal: "gallery-preview" }, {
+                                     skin: skin.url,
+                                     isSlim: skin.isSlim
+                                 });
+                             }}
                              onPointerEnter={() => selectSkin(index)}
                              alt=""
                         />
-                    )
+                    ))
                 }
                 {
                     !user.textures.skin.loaded && <Spinner className="Skin-Head" size="regular"/>
