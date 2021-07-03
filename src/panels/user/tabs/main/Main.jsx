2c34290444367bc8af9e436e8de584ea525f7915
Egor <mrzillagold@gmail.com>
2021-07-03 12:24:13 +1000

    chore(Main): redesign

--- src/panels/user/tabs/main/Main.jsx
+++ src/panels/user/tabs/main/Main.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useReducer, useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Input, FormLayout, FormItem, FormLayoutGroup, SimpleCell } from "@vkontakte/vkui";
-import { Icon24Copy, Icon24Done } from "@vkontakte/icons";
+import { MiniInfoCell, UsersStack, Spacing } from "@vkontakte/vkui";
+import { Icon20CommunityName, Icon20GameOutline, Icon20LaptopOutline, Icon20PacmanOutline, Icon20PinOutline, Icon20StatisticsOutline, Icon20Users, Icon20ViewOutline, Icon24Done } from "@vkontakte/icons";
 
-import { declOfNum, humanizeDuration, timeConvert } from "../../../../utils";
+import { IconServer } from "../../../../icons";
+
+import { ASHCON_ENDPOINT, declOfNum, humanizeDuration, timeConvert } from "../../../../utils";
 
 import "../../../hypixel/Info.css";
 
@@ -34,110 +36,133 @@ export function Main({ user: { uuid, views, friends, servers, badlion }, setActi
     const clearedUUID = uuid.replaceAll("-", "");
 
     return (
-        <FormLayout>
-            <FormLayoutGroup mode="horizontal">
-                <FormItem top="UUID">
-                    <Input value={uuid}
-                           after={
-                               <CopyToClipboard text={uuid}>
-                                   <div onClick={() => setCopied({ uuid: true })}>
-                                       {
-                                           copied.uuid ?
-                                               <Icon24Done/>
-                                               :
-                                               <Icon24Copy/>
-                                       }
-                                   </div>
-                               </CopyToClipboard>
-                           }
-                           readOnly
-                    />
-                </FormItem>
-                <FormItem top="Очищенный UUID">
-                    <Input value={clearedUUID}
-                           after={
-                               <CopyToClipboard text={clearedUUID}>
-                                   <div onClick={() => setCopied({ clearedUuid: true })}>
-                                       {
-                                           copied.clearedUuid ?
-                                               <Icon24Done/>
-                                               :
-                                               <Icon24Copy/>
-                                       }
-                                   </div>
-                               </CopyToClipboard>
-                           }
-                           readOnly
-                    />
-                </FormItem>
-            </FormLayoutGroup>
-            <div className="Info-List">
-                <SimpleCell description={declOfNum(views, ["Просмотр", "Просмотра", "Просмотров"])}
-                            disabled
+        <>
+            <CopyToClipboard text={uuid}
+                             onClick={() => setCopied({ uuid: true })}
+            >
+                <MiniInfoCell
+                    textLevel="primary"
+                    before={
+                        copied.uuid ?
+                            <Icon24Done width={20} height={20}/>
+                            :
+                            <Icon20CommunityName/>
+                    }
+                    mode="add"
+                    onClick={() => setCopied({ uuid: true })}
                 >
-                    { views }
-                </SimpleCell>
-                <SimpleCell description={
-                    declOfNum(friends.length, ["Друг", "Друга", "Друзей"])
+                    UUID: {uuid}
+                </MiniInfoCell>
+            </CopyToClipboard>
+            <CopyToClipboard text={clearedUUID}
+                             onClick={() => setCopied({ uuid: true })}
+            >
+                <MiniInfoCell
+                    textLevel="primary"
+                    before={
+                        copied.clearedUuid ?
+                            <Icon24Done width={20} height={20}/>
+                            :
+                            <Icon20CommunityName/>
+                    }
+                    mode="add"
+                    onClick={() => setCopied({ clearedUuid: true })}
+                >
+                    Очищенный UUID: {clearedUUID}
+                </MiniInfoCell>
+            </CopyToClipboard>
+            <Spacing separator/>
+            <MiniInfoCell
+                before={
+                    <Icon20Users width={20} height={20}/>
                 }
-                            onClick={() => setActiveTab("friends")}
-                >
-                    { friends.length }
-                </SimpleCell>
-                <SimpleCell description={
-                    declOfNum(servers.length, ["Любимый сервер", "Любимых сервера", "Любимых серверов"])
-                }
-                            onClick={() => setActiveTab("servers")}
-                >
-                    { servers.length }
-                </SimpleCell>
-            </div>
-            {
-                badlion &&
-                <>
-                    <div className="Info-List">
-                        <SimpleCell description="Последний онлайн"
-                                    disabled
-                        >
-                            { timeConvert(badlion.last_online) }
-                            <br/>
-                            { new Date(badlion.last_online).toLocaleTimeString() }
-                        </SimpleCell>
-                        <SimpleCell description="Наиграно"
-                                    disabled
-                        >
-                            { humanizeDuration(badlion.play_time) }
-                        </SimpleCell>
-                        <SimpleCell description="Версия"
-                                    disabled
-                        >
-                            { badlion.version }
-                        </SimpleCell>
-                    </div>
-                    <div className="Info-List">
-                        <SimpleCell description="Последний сервер"
-                                    disabled
-                        >
-                            {
-                                badlion.last_server === "Hidden" ?
-                                    "Скрыт"
-                                    :
-                                    badlion.last_server
-                            }
-                        </SimpleCell>
-                        {
-                            Boolean(badlion.login_streak.current || badlion.login_streak.max) &&
-                            <SimpleCell description="Входов подряд"
-                                        disabled
-                            >
-                                Сейчас: { badlion.login_streak.current }
-                                <br/>
-                                Максимум: { badlion.login_streak.max }
-                            </SimpleCell>
+                mode="more"
+                onClick={() => setActiveTab("friends")}
+                after={
+                    <UsersStack
+                        photos={
+                            friends.slice(0, 3).map(({ uuid }) => (
+                                `${ASHCON_ENDPOINT}/avatar/${uuid}`
+                            ))
                         }
-                    </div>
+                    />
+                }
+            >
+                {friends.length} {declOfNum(friends.length, ["друг", "друга", "друзей"])}
+            </MiniInfoCell>
+            <MiniInfoCell
+                before={
+                    <IconServer width={20} height={20}/>
+                }
+                mode="more"
+                onClick={() => setActiveTab("servers")}
+                after={
+                    <UsersStack
+                        photos={
+                            servers.slice(0, 3).map(({ icon }) => icon)
+                        }
+                    />
+                }
+            >
+                {servers.length} {declOfNum(servers.length, ["любимый сервер", "любимых сервера", "любимых серверов"])}
+            </MiniInfoCell>
+            <Spacing separator/>
+            <MiniInfoCell
+                textLevel="primary"
+                before={
+                    <Icon20ViewOutline/>
+                }
+            >
+                {views} {declOfNum(views, ["просмотр", "просмотра", "просмотров"])}
+            </MiniInfoCell>
+            {
+                badlion && <>
+                    <MiniInfoCell
+                        textLevel="primary"
+                        before={
+                            <Icon20LaptopOutline/>
+                        }
+                    >
+                        Последний онлайн { timeConvert(badlion.last_online) } в { new Date(badlion.last_online).toLocaleTimeString() }
+                    </MiniInfoCell>
+                    <MiniInfoCell
+                        textLevel="primary"
+                        before={
+                            <Icon20GameOutline/>
+                        }
+                    >
+                        Наиграно { humanizeDuration(badlion.play_time) }
+                    </MiniInfoCell>
+                    <MiniInfoCell
+                        textLevel="primary"
+                        before={
+                            <Icon20PinOutline/>
+                        }
+                    >
+                        Версия { badlion.version }
+                    </MiniInfoCell>
+                    <MiniInfoCell
+                        textLevel="primary"
+                        before={
+                            <Icon20PacmanOutline/>
+                        }
+                    >
+                        Последний сервер { badlion.last_server || "скрыт" }
+                    </MiniInfoCell>
+                    {
+                        Boolean(badlion.login_streak.current || badlion.login_streak.max) && (
+                            <MiniInfoCell
+                                textLevel="primary"
+                                before={
+                                    <Icon20StatisticsOutline/>
+                                }
+                            >
+                                Входов подряд: { badlion.login_streak.current } ({ badlion.login_streak.max })
+                            </MiniInfoCell>
+                        )
+                    }
                 </>
             }
-        </FormLayout>
+        </>
     );
 }
